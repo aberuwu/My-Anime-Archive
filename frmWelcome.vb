11225a1736505c5fcf815bfb497ff4c1e19fdcaf
Cesar Irad Mendoza <51037899+aberuwu@users.noreply.github.com>
2019-05-25 21:22:32 -0500

    Add files via upload

---
+++ frmWelcome.vb
@@ -0,0 +1,23 @@
+﻿Public Class frmWelcome
+    Private Sub frmWelcome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblWelcome.Text = "Welcome " & frmMain.userList(frmMain.userCount).Username & "!"
+
+        Dim USER_IMG_URL As String = "https://cdn.myanimelist.net/images/userimages/" & frmMain.userList(frmMain.userCount).UserId & ".jpg"
+
+        If frmMain.CheckConnection(USER_IMG_URL) = True Then
+            pcbUserImage.Load(USER_IMG_URL)
+        End If
+
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Me.Close()
+    End Sub
+
+    Private Sub lnkChangeLog_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkChangeLog.LinkClicked
+        Dim changelog As frmChangeLog
+
+        changelog = New frmChangeLog
+        changelog.ShowDialog()
+    End Sub
+End Class
---
+++ frmXml.vb
@@ -0,0 +1,47 @@
+﻿Public Class frmXml
+
+    Private drag As Boolean
+    Private mousex As Integer
+    Private mousey As Integer
+    Private maximize As Boolean
+    Private minimize As Boolean
+
+    Private Sub frmXml_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rtbXml.Text = System.IO.File.ReadAllText(frmMain.ANIME_SOURCE_XML)
+        rtbXml.Select(0, 0)
+        'tbXmlMain.
+        lblXmlFile.Text = frmMain.ANIME_SOURCE_XML
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmXml_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        drag = True
+        mousex = Windows.Forms.Cursor.Position.X - Me.Left
+        mousey = Windows.Forms.Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub frmXml_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub frmXml_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub lblMaximize_Click(sender As Object, e As EventArgs) Handles lblMaximize.Click
+
+        If maximize = False Then
+            maximize = True
+            Me.WindowState = FormWindowState.Maximized
+        Else
+            maximize = False
+            Me.WindowState = FormWindowState.Normal
+        End If
+    End Sub
+End Class
